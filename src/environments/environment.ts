f5b03f052b203af4b8cdcee7c1b436baac1ad7ae
Juan Carvajal Núñez <ju.carvajaln@duocuc.cl>
2026-09-12 21:19:47 -0300

    fix: redirectUri dinámico para Github Pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   clientId: (window as any).__env?.CLIENT_ID || 'd58f7e12-14df-458c-b966-507316adb29c',
   tenantId: (window as any).__env?.TENANT_ID || '4ecc8427-8a7b-46cc-aab9-4f000ea95b2e',
-  redirectUri: window.location.origin,
+  redirectUri: window.location.hostname === 'localhost'
+    ? 'http://localhost:4200'
+    : 'https://patatan7.github.io/pedidos360-frontend',
   apiUrl: (window as any).__env?.API_URL || 'https://bdmbgxdi95.execute-api.us-east-1.amazonaws.com',
 }
