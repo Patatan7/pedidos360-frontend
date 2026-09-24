03df199519fc3f26429c8a997a3c85392bffb813
Patatan7 <Patatan7@users.noreply.github.com>
2026-09-24 23:59:48 +0000

    deploy: ac5a24d056159ca56ecc041e223e2a2c32510c5f

---
+++ chunk-WB2OB5FM.js
@@ -0,0 +1,1 @@
+var a={clientId:window.__env?.CLIENT_ID||"d58f7e12-14df-458c-b966-507316adb29c",tenantId:window.__env?.TENANT_ID||"4ecc8427-8a7b-46cc-aab9-4f000ea95b2e",redirectUri:window.location.hostname==="localhost"?"http://localhost:4200":"https://patatan7.github.io/pedidos360-frontend",apiUrl:window.__env?.API_URL||"https://bdmbgxdi95.execute-api.us-east-1.amazonaws.com"};export{a};
